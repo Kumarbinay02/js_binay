efacfcd15c85650c9795e59db21d9670f1b3b840
Kumarbinay02 <binaykumarjena2005@gmail.com>
2025-10-20 21:16:42 +0530

    introduction abou datatypes in javascript

---
+++ 01_basiccs/02_dataTypes.js
@@ -0,0 +1,34 @@
+// "use strict"; treat all javascript code as newer version
+
+// alert("Hello World!");
+// alert(3+3) 
+//   Here we are using node js not browser so it is not executed and gives error
+
+
+console.log("hello Binay");
+
+// code rediability so high
+
+console.log(3+3);
+
+let name = "Binay Kumar Jena";
+let age = 19;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// BigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable without value
+// symbol
+
+let state = null;
+
+// object
+
+console.log(typeof "Binay kumar jena");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+ console.log(typeof null); //object
+console.log(typeof undefined); //undefined
